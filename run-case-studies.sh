5ddc0189b93c2bc6bb3a9fed5ea6d63d24276148
Renzo Degiovanni <rdegiovanni@dc.exa.unrc.edu.ar>
2017-08-11 22:08:02 -0300

    case studies added

---
+++ run-case-studies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#./case-studies/minepump/minepump.sh
+#./case-studies/atm/atm.sh
+./case-studies/telephone/telephone.sh
+./case-studies/railroad/railroad.sh
+./case-studies/tcp/tcp.sh
+#./case-studies/las/las.sh
