11295c71b82a101a315a043878169512dbe6033d
Renzo Degiovanni <rdegiovanni@dc.exa.unrc.edu.ar>
2017-11-16 07:37:12 -0300

    rebuttal example added

---
+++ case-studies/rebuttal/rebuttal.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+java -Xmx8g -Djava.library.path=/usr/local/lib -cp bin/.:lib/* main.Main -ltl="True" -alph="[p,q]" -all -k=100 -out=case-studies/rebuttal/DOM.txt
+
+java -Xmx8g -Djava.library.path=/usr/local/lib -cp bin/.:lib/* main.Main -bc="F(p && q)" -alph="[p,q]" -all -k=100 -out=case-studies/rebuttal/BC1.txt
+
+java -Xmx8g -Djava.library.path=/usr/local/lib -cp bin/.:lib/* main.Main -bc="F(! p)" -alph="[p,q]" -all -k=100 -out=case-studies/rebuttal/BC2.txt
